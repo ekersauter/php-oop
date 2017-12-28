3431dc8bde84acc2d313dc18b89aa15ea08256a7
Erik Sauter <erik.sauter@unleashed.be>
2017-12-28 11:15:36 +0100

    Create default.php

---
+++ default.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+		}
+
+		.main {
+			width: 94%;
+			position: relative;
+			margin-left: 10px;
+			margin-top: 10px;
+		}
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h2 {
+			font-size: 24px;
+			margin: 16px 0 0 0;
+			color: #00334d;
+		}
+	</style>
+</head>
+<body>
+	<div class="main">
+		
+		<?php
+
+			include("/methods.php");
+			include("/parser.php");
+
+		?>
+		
+	</div>
+</body>
+</html>
